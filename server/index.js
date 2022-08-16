034cf9d708756bb9d58b208d114f8c57e6acc6e0
Kelimazing <kafrancisco4@gmail.com>
2022-08-16 22:22:42 +0800

    Link Shortener v1.0.1

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use(
 if (process.env.NODE_ENV == 'production') {
   app.use(express.static('client/build'));
 
-  app.get('*', (req, ers) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
 }
